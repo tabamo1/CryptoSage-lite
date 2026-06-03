c7ec774dd0986e10b91ef21d813f3b82b683e8f0
tabamo1 <bryan.tabz@gmail.com>
2026-06-03 08:32:07 +0800

    Extract Flutter app source

---
+++ lib/theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+ThemeData appTheme() {
+  return ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF00B894)),
+    useMaterial3: true,
+    appBarTheme: const AppBarTheme(centerTitle: true),
+    cardTheme: const CardTheme(
+      elevation: 2,
+      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(16))),
+    ),
+  );
+}
