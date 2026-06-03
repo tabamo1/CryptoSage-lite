9af2dcdfc76af7035dbfa0a9a7dca550b7af99a3
tabamo1 <bryan.tabz@gmail.com>
2026-06-03 08:34:40 +0800

    Use current Flutter card theme API

--- lib/theme.dart
+++ lib/theme.dart
@@ -5,7 +5,7 @@ ThemeData appTheme() {
     colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF00B894)),
     useMaterial3: true,
     appBarTheme: const AppBarTheme(centerTitle: true),
-    cardTheme: const CardTheme(
+    cardTheme: const CardThemeData(
       elevation: 2,
       margin: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(16))),
